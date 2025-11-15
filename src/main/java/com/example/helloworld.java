7d67b8ecd678a157315af464239b87003639cba3
Prajusha <prajushaa@yahoo.com>
2025-11-15 20:27:49 +0530

    Update helloworld.java

--- src/main/java/com/example/helloworld.java
+++ src/main/java/com/example/helloworld.java
@@ -1,5 +1,5 @@
 package com.example;
-public class HelloWorld {
+public class helloworld {
 public static void main(String[] args) {
   System.out.println("Hello, DevOps CI/CD using GitHub Actions!");
   System.out.println("Change #1: Verifying automatic workflow trigger on push.");
